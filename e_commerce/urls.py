03be8fca3a4b6c024e8a00f20ac2aa4378662275
José Henrique Martins Pinheiro <pinheiro012345@gmail.com>
2023-08-27 12:10:44 -0300

    new pages

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import *
 
 
 urlpatterns = [
     path('', home),
+    path('about/', about),
+    path('contato/', contato),
     path('admin/', admin.site.urls),
 ]
--- e_commerce/views.py
+++ e_commerce/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Home Page",
+        "content": "Bem-vindo a pagina principal"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about(request):
+    context = {
+        "title": "About Page",
+        "content": "Bem-vindo a pagina Sobre"
+    }
+    return render(request, "about/view.html", context)
+
+
+def contato(request):
+    context = {
+        "title": "Pagina de contato",
+        "content": "Bem-vindo a pagina Contato"
+    }
+    return render(request, "contato/view.html", context)
